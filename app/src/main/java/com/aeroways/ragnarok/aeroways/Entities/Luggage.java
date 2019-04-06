0fde39cd25e5b9e2057092007e34c573b7eb5863
trikialaa <36111710+trikialaa@users.noreply.github.com>
2019-04-06 13:56:32 +0100

    Added Luggage class

---
+++ app/src/main/java/com/aeroways/ragnarok/aeroways/Entities/Luggage.java
@@ -0,0 +1,64 @@
+package com.aeroways.ragnarok.aeroways.Entities;
+
+public class Luggage {
+
+    private String id;
+
+    private String type;
+
+    private String color;
+
+    private double weight;
+
+    private String status;
+
+    private String lastSeen;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLastSeen() {
+        return lastSeen;
+    }
+
+    public void setLastSeen(String lastSeen) {
+        this.lastSeen = lastSeen;
+    }
+}
